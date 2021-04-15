23a9be17cf5c350f3f955a7eda4b39c69171273e
mikpi86 <piras.michela86@gmail.com>
2021-04-15 15:55:00 +0200

    footer

--- js/script.js
+++ js/script.js
@@ -19,6 +19,37 @@ var app = new Vue ({
 
             footerShop: 'img/avadabarbers_footer_shop_image-400x312.png',
 
+            footerCenter: {
+
+                footerLogo: 'img/avadabarbers-stickylogo-x2.png',
+
+                address: {
+
+                    name: 'Avada Barbers',
+
+                    street: '123 New York Street',
+
+                    city: 'New York City',
+
+                    eMail: 'info@yourwebsite.com',
+
+                    phone: '+1 (555) 555-1212'
+
+                },
+
+                socialIcons: {
+
+                    facebook: 'fab fa-facebook-f',
+
+                    twitter: 'fab fa-twitter',
+
+                    youtube: 'fab fa-youtube',
+
+                    instagram: 'fab fa-instagram'
+                }
+
+            },
+
             footerAppointment: 'img/avadabarbers_footer_appointment_image-400x312.png'
 
         }
